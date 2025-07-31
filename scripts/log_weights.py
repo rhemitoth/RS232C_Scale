4245d5c5cec9f63681f17dcd8485a6050d45d830
rhemi <rhemitoth@gmail.com>
2025-07-31 16:31:04 -0400

    updates

--- scripts/log_weights.py
+++ scripts/log_weights.py
@@ -77,7 +77,7 @@ def display_fat_deer_message(weight, epd):
 
     # Draw weight
     draw.text((25, 60), f"{int(weight):d}", font=large_font, fill=0)
-    draw.text((145, 40), "kg", font=small_font, fill=0)
+    draw.text((145, 60), "kg", font=small_font, fill=0)
 
     epd.display(epd.getbuffer(image))
 
