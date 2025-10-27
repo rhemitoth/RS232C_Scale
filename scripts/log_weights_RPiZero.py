96d9ac6815d9a542bf1be5e49a256fd52454d4d8
rhemi <rhemitoth@gmail.com>
2025-10-27 16:16:37 -0400

    hasdf

--- scripts/log_weights_RPiZero.py
+++ scripts/log_weights_RPiZero.py
@@ -87,7 +87,7 @@ try:
         line = ser.readline().decode("utf-8", errors="ignore").strip()
         print(line)
         mount_path = get_usb_mount_path()
-        print(f"USB Mount Path: {mount_path}")
+        #print(f"USB Mount Path: {mount_path}")
         if line:
             result = parse_weight_line(line)
             if result:
