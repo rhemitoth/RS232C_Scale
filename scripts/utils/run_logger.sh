87d52d42c8cad5df04352f891d93dd916886a926
rhemi <rhemitoth@gmail.com>
2025-10-29 10:55:30 -0400

    updating service

---
+++ scripts/utils/run_logger.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# run_logger.sh
+
+# Activate virtual environment
+source /home/moorcroftlab/scale/bin/activate
+
+# Run the script
+exec python /home/moorcroftlab/Documents/RS232C_Scale/scripts/log_weights_RPiZero.py
